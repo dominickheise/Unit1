370b84cbf31710afae6372c4f1444a0b61e0fb31
dominickheise <826741@mystma.org>
2020-04-08 15:12:49 +0000

    Added movie list

---
+++ Unit1/src/app/App.java
@@ -0,0 +1,11 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+     System.out.println("1. Jojo Rabbit");
+        System.out.println("2. Parasite");
+        System.out.println("3. Empire Strikes Back");
+        System.out.println("4. Revenge of the Sith");
+        System.out.println("5. Jay and Silent Bob Rebooted");
+    }
+}
