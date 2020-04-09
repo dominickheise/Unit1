d998daea402730bdbe9b7798a71a020e33f8561e
dominickheise <826741@mystma.org>
2020-04-09 16:18:11 +0000

    added variables

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,10 +2,30 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-     System.out.println("1. Jojo Rabbit");
-        System.out.println("2. Parasite");
-        System.out.println("3. Empire Strikes Back");
-        System.out.println("4. Revenge of the Sith");
-        System.out.println("5. Jay and Silent Bob Rebooted");
+   
+    boolean AmIAtHome = true;
+    byte MultipleOf8 = 72;
+    char FirstLetterOfStateILiveIn = 'M';
+    short HeadsetBatteryLifeMin = 960;
+    int PopulationOfChina = 1439323776;
+    long ObservableStars = 100000000000L;
+    float DistanceLightYearsTOCLosestStar = 4.22F;
+    double RandomNumber = 4.6582415671295364D;
+    System.out.println("Am I at home");
+    System.out.println(AmIAtHome);
+    System.out.println("First letter of home state");
+    System.out.println(FirstLetterOfStateILiveIn);
+    System.out.println("Multiple of 8");
+    System.out.println(MultipleOf8);
+    System.out.println("Battery life of headset in minutes");
+    System.out.println(HeadsetBatteryLifeMin);
+    System.out.println("Population of China");
+    System.out.println(PopulationOfChina);
+    System.out.println("Number of observable stars");
+    System.out.println(ObservableStars);
+    System.out.println("Distance in light years to closest star");
+    System.out.println(DistanceLightYearsTOCLosestStar);
+    System.out.println("Random number");
+    System.out.println(RandomNumber);        
     }
 }
