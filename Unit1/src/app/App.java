223fbd7df743ca22cc074c7e6b9f94f2fe3fd12e
dominickheise <826741@mystma.org>
2020-04-16 16:40:58 +0000

    for loop mastery

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -5,26 +5,21 @@ import javax.net.ssl.SSLKeyException;
 public class App {
     public static void main(String[] args) throws Exception{
    
-  for (int i = 10; i <= 20; i = i +1){
-      System.out.println("The count is " + i);
-      System.out.println("Let's keep counting");
+  for (int i = 22; i <= 28; i = i +3){
+      System.out.println(i);
 }
-for (int i = 10; i <= 100; i = i +10){
-    System.out.println("The count is " + i);
-    System.out.println("Let's keep counting");
+for (int i = 2; i <= 32; i = i *2){
+    System.out.println(i);
 }
-for (int i = 1000; i <= 256000; i = i +2000){
-    System.out.println("The count is " + i);
-    System.out.println("Let's keep going");
+for (int i = 3; i >= -81; i = i *-i) {
+     System.out.println(+ i);
 }
-for (double i = 11; i <= 12; i = i + 0.1) {
-    System.out.println("The count is " + i);
-    System.out.println("Let's keep counting");
+for (int i = 50; i >= 5; i = i -5){
+    System.out.println(i);
 }
-for (int i = 10; i >= 0; i = i -1){
-    System.out.println("The count is " + i);
-    System.out.println("Let's keep counting");
-}
-
+int [] intArray = {1, 4, 1, 4, 5, 2, 9, 6, 3, 16, 7, 4, 25, 8, 5, 36, 9, 6, 49, 10, 7, 64, 11, 8, 81, 12, 9};
+    for(int i=0; i<intArray.length; i++) {
+      System.out.println(intArray[i]);
+    }
 }
 }
