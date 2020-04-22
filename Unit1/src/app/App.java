fb6124d509f769a8c141d1745d286eeec514c761
dominickheise <826741@mystma.org>
2020-04-22 15:30:05 +0000

    finished second loop challenge

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -5,20 +5,22 @@ import javax.net.ssl.SSLKeyException;
 public class App {
     public static void main(String[] args) throws Exception{
    System.out.println("Start first loop");
-   for (int x = 0; x <= 19; x++){
+   for (int x = 20; x >= 0; x = x - 1){
+       System.out.println(x);
+       if (x == 10) 
+            for (int y = 0; y < 7; y++) 
        System.out.println(x);
    }
 
 
    System.out.println("End first loop");
    System.out.println("Start second loop");
-   for (int x = -1; x <= 19; x++){
-       if (x % 2 == 0) {
-           System.out.println(x);
-       }else {
-           System.out.println(x++);
-        }
-       
+   for (int x = 4; x <= 36; x = x + 4){
+       System.out.println(x);
+       if (x % 12 == 0){
+           System.out.println("Hey!");
+           
+       }
    }
    System.out.println("End second loop");
 
