5fbc107c1e26123a005305af99219f31891b2477
dominickheise <826741@mystma.org>
2020-04-24 15:36:48 +0000

    Added 4/24 Quiz

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -4,23 +4,23 @@ import javax.net.ssl.SSLKeyException;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Double or single digit?");
-        for (int x = 20; x >= 1; x = x - 1) {
-            if (x >= 10)
-                System.out.println(x + " is a double digit");
-            else
-                System.out.println(x + " is a single digit");
+        System.out.println("Start first loop");
+        for (int x = 1; x <= 10; x++) {
+            if (x == 5){
+                System.out.println("five");
+        }else System.out.println(x);      
+    }
+        System.out.println("End first loop");
+        System.out.println("Start second loop");
+        for (int x = 100; x <= 200; x = x +10) {
+            System.out.println(x);
+            if (x == 150){
+                for (int y = 150; y <= 159; y++){
+                    System.out.println(y);
+                }
+            }
         }
-        System.out.println("Nice!");
-        System.out.println("Fast food");
-        for (int x = 1; x <= 7; x++) {
-            if (x % 2 == 0) {
-                System.out.println("Burgers");
-
-            } else
-                System.out.println("Fries");
-        }
-        System.out.println("Nice!");
+        System.out.println("End second loop");
 
     }
 }
