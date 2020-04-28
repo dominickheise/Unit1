dd42d94aa6a2595e34ea95b0868f43ced9a327e9
dominickheise <826741@mystma.org>
2020-04-28 16:21:39 +0000

    added input statements

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -1,26 +1,19 @@
 package app;
-
-import javax.net.ssl.SSLKeyException;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Start first loop");
-        for (int x = 1; x <= 10; x++) {
-            if (x == 5){
-                System.out.println("five");
-        }else System.out.println(x);      
-    }
-        System.out.println("End first loop");
-        System.out.println("Start second loop");
-        for (int x = 100; x <= 200; x = x +10) {
-            System.out.println(x);
-            if (x == 150){
-                for (int y = 150; y <= 159; y++){
-                    System.out.println(y);
-                }
-            }
-        }
-        System.out.println("End second loop");
+        Scanner input = new Scanner(System.in);
 
-    }
-}
+        System.out.println("Please enter your first name.");
+        String firstname = input.nextLine();
+        System.out.println("Please enter your last name.");
+        String lastname = input.nextLine();
+        System.out.println("Please enter your age.");
+        String age = input.nextLine();
+        System.out.println("Please enter your grade.");
+        String grade = input.nextLine();
+        System.out.println("Please enter your favorite colour.");
+        String color = input.nextLine();
+        System.out.println("You are " + firstname + " " + lastname + ", you are " + age + " years old and in grade " + grade + ". Your favorite colour is " + color  + ".");
+}}
