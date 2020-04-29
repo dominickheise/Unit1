3d1f85f17d6e1e2d93e8963f53f25ae551b8ee3a
dominickheise <826741@mystma.org>
2020-04-29 15:31:49 +0000

    Added password statement

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -4,16 +4,12 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
-
-        System.out.println("Please enter your first name.");
-        String firstname = input.nextLine();
-        System.out.println("Please enter your last name.");
-        String lastname = input.nextLine();
-        System.out.println("Please enter your age.");
-        String age = input.nextLine();
-        System.out.println("Please enter your grade.");
-        String grade = input.nextLine();
-        System.out.println("Please enter your favorite colour.");
-        String color = input.nextLine();
-        System.out.println("You are " + firstname + " " + lastname + ", you are " + age + " years old and in grade " + grade + ". Your favorite colour is " + color  + ".");
-}}
+        
+        System.out.println("Please enter password.");
+        String password = input.nextLine();
+        if (password.equals("183726073"))
+        System.out.println("ACCESS GRANTED");
+        else System.out.println("ACCESS DENIED");
+        
+   
+    }}      
