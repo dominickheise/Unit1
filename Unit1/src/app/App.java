d2cd2f236b2f0de842a17bef6f8fb82cb176fdbb
dominickheise <826741@mystma.org>
2020-04-30 15:06:45 +0000

    added count

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -5,11 +5,11 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
         
-        System.out.println("Please enter password.");
-        String password = input.nextLine();
-        if (password.equals("183726073"))
-        System.out.println("ACCESS GRANTED");
-        else System.out.println("ACCESS DENIED");
+        System.out.println(">What is the count?");
+        int number = input.nextInt();
+       for (int n = 0; n <= number; n++){
+           System.out.println(">" + n);
+       }
         
    
     }}      
