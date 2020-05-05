f14fe8758a62c5b4936136ccd3b98f18c8e1cf3a
dominickheise <826741@mystma.org>
2020-05-05 15:31:06 +0000

    Added 3 questions

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -5,11 +5,19 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
         
-        System.out.println(">What is the count?");
-        int number = input.nextInt();
-       for (int n = 0; n <= number; n++){
-           System.out.println(">" + n);
-       }
-        
-   
+        System.out.println("What is my name?");
+        String Q1 = input.nextLine();
+        if (Q1.equals("Dominick"))
+            System.out.println("CORRECT");
+        else System.out.println("INCORRECT");
+         System.out.println("What is the capital city of Korea");
+        String Q2 = input.nextLine();
+        if (Q2.equals("Seoul"))
+            System.out.println("CORRECT");
+        else System.out.println("INCORRECT");
+         System.out.println("What month is it?");
+        String Q3 = input.nextLine();
+        if (Q3.equals("May"))
+            System.out.println("CORRECT");
+        else System.out.println("INCORRECT");
     }}      
