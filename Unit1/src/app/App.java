db7d9565eab11a20440215a9792dbc9cf64c4e49
dominickheise <826741@mystma.org>
2020-05-08 17:09:16 +0000

    added psuedo-calculator

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -5,25 +5,27 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
         
-        System.out.println("Enter a first name.");
-        String Q1 = input.nextLine();
-        System.out.println("Enter a last name.");
-        String Q2 = input.nextLine();
-        System.out.println("Enter an Illness");
-        String Q3 = input.nextLine();
-        System.out.println("Enter a plural noun.");
-        String Q4 = input.nextLine();
-        System.out.println("Enter an adjective.");
-        String Q5 = input.nextLine();
-        System.out.println("Enter an adjective.");
-        String Q6 = input.nextLine();
-        System.out.println("Enter a silly word.");
-        String Q7 = input.nextLine();
-        System.out.println("Enter a place.");
-        String Q8 = input.nextLine();
         System.out.println("Enter a number.");
-        String Q9 = input.nextLine();
-        System.out.println("Enter an adjective.");
-        String Q10 = input.nextLine();
-        System.out.println("Dear School Nurse:" + Q1 + " " + Q2 + " will not be attending school today. He/she has come down with a case of " + Q3 + " and has horrible " + Q4 + " and a/an " + Q5 + " fever. We have made an appointment with the " + Q6 + " Dr. " + Q7 +  " who studied for many years in " + Q8 + " and has " + Q9 + " degrees in pediatrics. He will send you all the information you need. Thank you! Sincerely Mrs. " + Q10);
+        int numb1 = input.nextInt();
+
+        System.out.println("Entaer another number");
+        int numb2 = input.nextInt();
+
+        System.out.println("Enter a function; press 1 for +, 2 for -, 3 for *, and 4 for /");
+        int func = input.nextInt();
+        if (func == 1){
+            System.out.println("+");
+        } 
+        if (func == 2){
+            System.out.println("-");
+        }
+        if (func == 3){
+            System.out.println("*");
+        }
+        if (func == 4){
+            System.out.println("/");
+        }
+
+        System.out.println(numb1 + func + numb2);
+
     }}      
