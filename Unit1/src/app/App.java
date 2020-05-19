b154e8b204190a4a33c2213bb81970fff1e20b18
dominickheise <826741@mystma.org>
2020-05-19 15:53:05 +0000

    added more variables

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,33 +3,41 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Dog dog1 = new Dog();
+        Dog dog2 = new Dog();
+        Dog dog3 = new Dog();
         Scanner input = new Scanner(System.in);
+        dog1.name = "Spot";
+        dog1.age = 2;
+        dog1.breed = "Shiba Inu";
+        dog1.size = "Small";
+        dog1.FurColor = "Golden Brown";
+        dog1.OwnerLastName = "Imagawa";
         
-        System.out.println("Enter a number.");
-        int numb1 = input.nextInt();
+        dog2.name = "Hunter";
+        dog2.age = 4;
+        dog2.breed = "German Shepard";
+        dog2.size = "Large";
+        dog2.FurColor = "Black and Brown";
+        dog2.OwnerLastName = "Jackson";
 
-        System.out.println("Enter another number");
-        int numb2 = input.nextInt();
+        dog3.name = "Ace";
+        dog3.age = 3;
+        dog3.breed = "Husky";
+        dog3.size = "Medium";
+        dog3.FurColor = "White and Grey";
+        dog3.OwnerLastName = "Anderson";
 
-        System.out.println("Enter a function; press 1 for +, 2 for -, 3 for *, and 4 for /");
-        int func = input.nextInt();
-        if (func == 1){
-            System.out.println(numb1 + "+" + numb2 + "=");
-            System.out.println(numb1 + func + numb2);
-        } 
-        if (func == 2){
-            System.out.println(numb1 + "-" + numb2 + "=");
-            System.out.println(numb1 + func + numb2);
-        }
-        if (func == 3){
-            System.out.println(numb1 + "*" + numb2 + "=");
-            System.out.println(numb1 + func + numb2);
-        }
-        if (func == 4){
-            System.out.println(numb1 + "/" + numb2 + "=");
-            System.out.println(numb1 + func + numb2);
-        }
+        System.out.println(dog1.name);
+        System.out.println(dog1.age);
+        System.out.println(dog1.breed);
 
-        
+        System.out.println(dog2.name);
+        System.out.println(dog2.age);
+        System.out.println(dog2.breed);
+
+        System.out.println(dog3.name);
+        System.out.println(dog3.age);
+        System.out.println(dog3.breed);
 
     }}      
---
+++ Unit1/src/app/Dog.java
@@ -0,0 +1,11 @@
+package app;
+
+public class Dog{
+
+    String name;
+    int age;
+    String breed;
+    String size;
+    String FurColor;
+    String OwnerLastName;
+}
