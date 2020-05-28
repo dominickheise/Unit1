94cfc5e1f119c3b6a34c54ffb5245979227f7fe5
dominickheise <826741@mystma.org>
2020-05-28 18:01:26 +0000

    Added more of bank system

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -5,12 +5,25 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         Scanner input = new Scanner(System.in);
+
         System.out.println("What is your name");
-        String name = input.nextLine();
+        String name1 = input.nextLine();
         System.out.println("How much money are you starting with");
-        int start = input.nextInt();
+        int cash = input.nextInt();
         System.out.println("Please create password");
-        String password = input.nextLine();
+        String pass1 = input.nextLine();
+        System.out.println("Account created log in");
+         System.out.println("Enter your name");
+        String name2 = input.nextLine();
+        System.out.println("Enter your password");
+        String pass2 = input.nextLine();
+        
+        if (name2.equals(name1)) {
+    } else System.out.println("Access denied");
+        if (pass2.equals(pass1)){
+            System.out.println("Access granted");
+        } else System.out.println("Access denied");
 
 
-    }}      
+
+    }}
